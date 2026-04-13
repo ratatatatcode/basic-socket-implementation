cda7092a973d6f55075388d1ed0499fa9c34ece7
ratatatatcode <pogisijamesmagnaye@gmail.com>
2026-04-13 21:59:05 +0800

    Initial backend setup

---
+++ backend/index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import http from "http";
+import { Server } from "socket.io";
+
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: process.env.FRONTEND_URL,
+        methods: ["GET", "POST"]
+    }
+});
+
+const PORT = process.env.PORT;
+server.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
